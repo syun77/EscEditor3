0b57ce8d4cbc2d97079b31b238763947466a386c
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-09-03 00:03:57 +0900

    都道府県問題をHTML5に反映

--- html5/haxe/ApplicationMain.hx
+++ html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "126";
+		app.meta["build"] = "127";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "EscapeGameEditor";
 		app.meta["name"] = "EscapeGameEditor";
