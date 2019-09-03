34d3c2aa89e39279bc724f7391c288f89153f6dd
Syunsuke Ozeki <2dgames.jp@gmail.com>
2019-09-03 21:39:34 +0900

    ゲートCの問題差替え。最終ゲートの問題差し替え

--- html5/haxe/ApplicationMain.hx
+++ html5/haxe/ApplicationMain.hx
@@ -32,7 +32,7 @@ class ApplicationMain
 
 		ManifestResources.init(config);
 
-		app.meta["build"] = "133";
+		app.meta["build"] = "136";
 		app.meta["company"] = "HaxeFlixel";
 		app.meta["file"] = "EscapeGameEditor";
 		app.meta["name"] = "EscapeGameEditor";
